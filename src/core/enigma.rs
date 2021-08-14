2cb93a570eb7c554efee5c6706c1a7d549f8e2c9
Thomas Andolf <thomas.andolf@gmail.com>
2021-08-14 22:15:13 +0200

    added a rotor_manager

--- src/core/enigma.rs
+++ src/core/enigma.rs
@@ -1,19 +1,20 @@
 use super::rotor::Rotor;
+use super::rotor_manager::{ReflectorType, RotorManager, RotorType};
 
 pub struct Enigma {
-    rotors: Vec<Rotor>,
+    rotor_manager: RotorManager,
 }
 
 impl Enigma {
     pub fn new() -> Self {
-        let set = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-        let rotor = Rotor::new(set, 'k');
-        let rotors = vec![rotor];
-        Self { rotors }
+        let rotor_manager = RotorManager::new(ReflectorType::TEST)
+            .add_rotor(RotorType::TEST)
+            .add_rotor(RotorType::TEST);
+        Self { rotor_manager }
     }
 
-    pub fn with_rotors(rotors: Vec<Rotor>) -> Self {
-        Self { rotors }
+    pub fn with_rotor_manager(rotor_manager: RotorManager) -> Self {
+        Self { rotor_manager }
     }
 
     pub fn encrypt(&mut self, value: &str) -> String {
@@ -21,32 +22,16 @@ impl Enigma {
         value.retain(|c| !c.is_whitespace());
         let mut enc_string = String::with_capacity(value.len());
 
-        // update rotors into position
-        self.update_rotors();
-        // make the connection
+        self.rotor_manager.update_rotors();
 
         for c in value.chars() {
             let mut c_under_enc = c;
-            for rotor in self.rotors.iter_mut().peekable() {
+            for rotor in self.rotor_manager.rotors.iter_mut().peekable() {
                 c_under_enc = rotor.encode(&c_under_enc);
             }
             enc_string.push(c_under_enc);
         }
         enc_string
-    }
-
-    fn update_rotors(&mut self) {
-        let mut rotate_next = true;
-        for rotor in self.rotors.iter_mut() {
-            let at_notch = rotor.at_notch();
-            if rotate_next {
-                rotor.rotate();
-            }
-
-            if !at_notch {
-                rotate_next = false;
-            }
-        }
     }
 }
 
@@ -57,38 +42,26 @@ mod test {
 
     #[test]
     pub fn enigma_one_rotor_with_rotation() {
-        let rotors = vec![Rotor::with_key("ABC")];
-        let mut enigma = Enigma::with_rotors(rotors);
-        assert_eq!("B", enigma.encrypt("a"));
+        let rotor_manager = RotorManager::new(ReflectorType::TEST).add_rotor(RotorType::TEST);
+        let mut enigma = Enigma::with_rotor_manager(rotor_manager);
+        assert_eq!(enigma.encrypt("a"), "B");
     }
 
     #[test]
     pub fn enigma_two_rotors_no_notch() {
-        let rotors = vec![Rotor::with_key("ABC"), Rotor::with_key("ABC")];
-        let mut enigma = Enigma::with_rotors(rotors);
+        let rotor_manager = RotorManager::new(ReflectorType::TEST)
+            .add_rotor(RotorType::TEST_NOTCHLESS)
+            .add_rotor(RotorType::TEST_NOTCHLESS);
+        let mut enigma = Enigma::with_rotor_manager(rotor_manager);
         assert_eq!(enigma.encrypt("a"), "B");
     }
 
     #[test]
     pub fn if_at_notch_should_rotate_second_rotor() {
-        let rotors = vec![Rotor::new("ABC", 'A'), Rotor::with_key("ABC")];
-        let mut enigma = Enigma::with_rotors(rotors);
-        enigma.update_rotors();
-        println!("{:?}", enigma.rotors);
-    }
-
-    #[test]
-    pub fn should_rotate_first_rotor_not_second() {
-        let rotors = vec![Rotor::new("ABC", 'B'), Rotor::with_key("ABC")];
-        let mut enigma = Enigma::with_rotors(rotors);
-
-        enigma.update_rotors();
-
-        let mut rotor_iter = enigma.rotors.iter_mut();
-        let first_rotor = rotor_iter.next().unwrap();
-        let second_rotor = rotor_iter.next().unwrap();
-
-        assert_eq!(first_rotor.state.pop_front(), Some('B'));
-        assert_eq!(second_rotor.state.pop_front(), Some('A'));
+        let rotor_manager = RotorManager::new(ReflectorType::TEST)
+            .add_rotor(RotorType::TEST)
+            .add_rotor(RotorType::TEST_NOTCHLESS);
+        let mut enigma = Enigma::with_rotor_manager(rotor_manager);
+        assert_eq!(enigma.encrypt("a"), "B");
     }
 }
--- src/core/rotor_manager.rs
+++ src/core/rotor_manager.rs
@@ -1,36 +1,39 @@
 use super::rotor::Rotor;
 
-#[allow(non_camel_case_types)]
-enum ReflectorType {
+#[allow(non_camel_case_types, dead_code)]
+pub enum ReflectorType {
     UKW_A,
     TEST,
 }
 
 impl ReflectorType {
     fn value(&self) -> Rotor {
-        match self {
+        match *self {
             ReflectorType::UKW_A => Rotor::with_key("EJMZALYXVBWFCRQUONTSPIKHGD"),
             ReflectorType::TEST => Rotor::with_key("ABC"),
         }
     }
 }
 
-enum RotorType {
+#[allow(dead_code)]
+pub enum RotorType {
     I,
     TEST,
+    TEST_NOTCHLESS,
 }
 
 impl RotorType {
     fn value(&self) -> Rotor {
-        match self {
+        match *self {
             RotorType::I => Rotor::new("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Y'),
-            RotorType::TEST => Rotor::new("", 'Y'),
+            RotorType::TEST => Rotor::new("ABC", 'B'),
+            RotorType::TEST_NOTCHLESS => Rotor::with_key("ABC"),
         }
     }
 }
 
-struct RotorManager {
-    rotors: Vec<Rotor>,
+pub struct RotorManager {
+    pub(super) rotors: Vec<Rotor>,
     reflector: Rotor,
 }
 
@@ -45,5 +48,64 @@ impl RotorManager {
     pub fn add_rotor(mut self, rotor_type: RotorType) -> Self {
         self.rotors.push(rotor_type.value());
         self
+    }
+
+    pub fn update_rotors(&mut self) {
+        let mut rotate_next = true;
+        for rotor in self.rotors.iter_mut() {
+            let at_notch = rotor.at_notch();
+            if rotate_next {
+                rotor.rotate();
+            }
+
+            if !at_notch {
+                rotate_next = false;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    pub fn should_add_rotor() {
+        let rotor_manager = RotorManager::new(ReflectorType::TEST).add_rotor(RotorType::TEST);
+        assert_eq!(rotor_manager.rotors.len(), 1)
+    }
+
+    #[test]
+    pub fn should_rotate_first_but_not_second() {
+        let mut rotor_manager = RotorManager::new(ReflectorType::TEST)
+            .add_rotor(RotorType::TEST)
+            .add_rotor(RotorType::TEST);
+
+        rotor_manager.update_rotors();
+
+        let mut rotors_iter = rotor_manager.rotors.iter();
+        let first_rotor = rotors_iter.next().unwrap();
+        let second_rotor = rotors_iter.next().unwrap();
+
+        assert_eq!(first_rotor.state.get(0).unwrap(), &'B');
+        assert_eq!(second_rotor.state.get(0).unwrap(), &'A');
+    }
+
+    #[test]
+    pub fn should_rotate_first_and_second() {
+        let mut rotor_manager = RotorManager::new(ReflectorType::TEST)
+            .add_rotor(RotorType::TEST)
+            .add_rotor(RotorType::TEST);
+
+        rotor_manager.update_rotors();
+        rotor_manager.update_rotors();
+
+        let mut rotors_iter = rotor_manager.rotors.iter();
+        let first_rotor = rotors_iter.next().unwrap();
+        let second_rotor = rotors_iter.next().unwrap();
+
+        assert_eq!(first_rotor.state.get(0).unwrap(), &'C');
+        assert_eq!(second_rotor.state.get(0).unwrap(), &'B');
     }
 }
